4ab16bb6c982505ef18970abaee7664f959379d7
michael <m.deimel@miceportal.com>
2024-01-09 14:16:19 +0100

    better styling

--- app/controllers/current_tracks_controller.rb
+++ app/controllers/current_tracks_controller.rb
@@ -1,8 +1,16 @@
 class CurrentTracksController < ApplicationController
   def index
     @station = StationPresenter.new Station.find(params[:station_id])
-    @current_track = RadioboxLastTrack.new(url: @station.radio_box_url).call if @station.radiobox.present?
 
-    @last_fm = LastFmApi.new(artist: @current_track.artist, title: @current_track.title).call if @current_track && params[:show_album_info] == "true"
+    if @station.radiobox.present?
+      @current_track = RadioboxLastTrack.new(url: @station.radio_box_url).call
+      # cache album infos for 24h
+      @last_fm = if @current_track && params[:show_album_info] == "true"
+        cache_key = "current_track/#{@current_track.artist}-#{@current_track.title}".parameterize
+        Rails.cache.fetch(cache_key, expires_in: 24.hours) do
+          LastFmApi.new(artist: @current_track.artist, title: @current_track.title).call
+        end
+      end
+    end
   end
 end
