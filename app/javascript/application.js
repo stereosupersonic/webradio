5796b9803638ff52768e4cf4bda567218b5277f6
michael <m.deimel@miceportal.com>
2024-01-09 14:11:45 +0100

    fix fontawesome

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,7 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 
+import "@fortawesome/fontawesome-free/js/all"
 import * as bootstrap from "bootstrap"
 
 import "./controllers"
