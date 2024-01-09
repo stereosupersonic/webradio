da7fe65818ba6cc59e048b5d6e77ab3ef51ff80f
michael <m.deimel@miceportal.com>
2024-01-09 15:41:40 +0100

    blah

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,8 @@ Rails.application.configure do
 
   # Enable server timing
   config.server_timing = true
+  config.server_timing = true
+  config.server_timing = true
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
