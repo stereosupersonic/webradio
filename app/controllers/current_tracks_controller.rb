bc3bb57b0c52bdbcfedc38a86952b25408b5e69a
michael <m.deimel@miceportal.com>
2024-01-17 20:58:58 +0100

    better loggng

--- app/controllers/current_tracks_controller.rb
+++ app/controllers/current_tracks_controller.rb
@@ -4,10 +4,12 @@ class CurrentTracksController < ApplicationController
 
     current_track_by_steam = StreamLastTrack.new(url: @station.url).call unless @station.ignore_tracks_from_stream?
 
-    Rails.logger.info "current_track_by: steam #{current_track_by_steam}" if current_track_by_steam
+    Rails.logger.info "#{@station.name} current_track_by: steam #{current_track_by_steam}" if current_track_by_steam
     @current_track = current_track_by_steam
     @current_track ||= RadioboxLastTrack.new(url: @station.radio_box_url).call
-    Rails.logger.info "current_track_by: radiobox #{@current_track}" if @current_track && !current_track_by_steam
+    Rails.logger.info "#{@station.name} current_track_by: radiobox #{@current_track}" if @current_track && !current_track_by_steam
+    Rails.logger.info "#{@station.name} no current_track_by" unless @current_track
+
 
     @last_fm = if @current_track && params[:show_album_info] == "true"
       Rails.cache.fetch("current_track/#{@current_track.key}") do
