0bd152967fe8eb7d717b723b3b5165db25f59140
michael <m.deimel@miceportal.com>
2024-01-19 13:50:33 +0100

    longer caching

--- app/controllers/track_infos_controller.rb
+++ app/controllers/track_infos_controller.rb
@@ -4,8 +4,10 @@ class TrackInfosController < ApplicationController
     @current_track = Rails.cache.read("current_track/#{@station.id}")
 
     @chat_gpt_said = if @current_track
+
       cache_key = "v1/current_track_chat_gpt/#{@current_track.key}".parameterize
-      Rails.cache.fetch(cache_key, expires_in: 2.weeks) do
+      Rails.logger.info "TrackInfosController: cache_key: #{cache_key}"
+      Rails.cache.fetch(cache_key, expires_in: 24.hours) do
         TrackInfoChatGpt.new(artist: @current_track.artist, title: @current_track.title).call
       end
     end
