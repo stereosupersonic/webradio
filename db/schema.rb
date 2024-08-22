2ac046b3ab6853f391274c31392a3fd7ba2dd6df
MICE Portal <michael@deimel.de>
2024-08-22 14:50:47 +0200

    ignore this

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_01_19_173650) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_22_120455) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
